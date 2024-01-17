41634484c4f0309e2a6c992cd38b20304bc5f365
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2024-01-16 19:29:57 -0500

    login cliente

--- cgi-bin/login_cliente.pl
+++ cgi-bin/login_cliente.pl
@@ -11,6 +11,7 @@
 #         ...
 #     </error>
 # </errors>
+# o solo la creacion de una cookie si todo esta bien :>
 
 use strict;
 use warnings;
@@ -25,41 +26,83 @@ my $card_number = $cgi->param("card_number");
 my $password = $cgi->param("password");
 my $session_time = 600;
 
-my $u = "query";
-my $p = "YR4AFJUC3nyRmasY";
-my $dsn = "dbi:mysql:database=bancafinal;host=127.0.0.1";
-my $dbh = DBI->connect($dsn, $u, $p);
+my $card_number_status = check_card_number($card_number);
+my $password_status = check_password($password);
+my %errors = (card_number => $card_number_status, password => $password_status);
 
-my $sth = $dbh->prepare("SELECT * FROM tarjetas WHERE numero=? AND clave=?");
-$sth->execute($number, $password);
 
-my @card = $sth->fetchrow_array;
-if (@card) {
-    $sth = $dbh->prepare("SELECT * FROM cuentas WHERE numero=?");
-    $sth->execute($number);
-    my @account = $sth->fetchrow_array;
-    $sth = $dbh->prepare("SELECT * FROM clientes WHERE id=?");
-    $sth->execute($account[6]);
-    my @client = $sth->fetchrow_array;
-    my $name = $client[2]." ".$client[3]." ".$client[4];
+login();
 
-    my $session = CGI::Session->new();
-    $session->param("name", $name);
-    $session->param("card_id", $card[0]);
-    $session->param("account_id", $account[0]);
-    $session->expire(time + $session_time);
-    $session->flush();
 
-    my $cookie = $cgi->cookie(-name => "session_id_cliente",
-                            -value => $session->id(),
-                            -expires => time + $session_time,
-                            "-max-age" => time + $session_time);
-    $cookie->bake();
+sub login {
+    if (!$card_number_status && !$password_status) {
+        my $u = "query";
+        my $p = "YR4AFJUC3nyRmasY";
+        my $dsn = "dbi:mysql:database=bancafinal;host=127.0.0.1";
+        my $dbh = DBI->connect($dsn, $u, $p);
 
-    print "correct";
-} else {
-    print($cgi->header("text/html"));
-    print "wrong";
+        my $sth = $dbh->prepare("SELECT `tarjetas`.`id`, `cuentas`.`id`, CONCAT(clientes.nombres, ' ', clientes.paterno, ' ', clientes.materno)
+                                FROM tarjetas, cuentas, clientes
+                                WHERE tarjetas.numero = '$card_number' AND tarjetas.clave = '$password' AND cuentas.tarjeta_id = tarjetas.id AND cuentas.cliente_id = clientes.id;");
+        $sth->execute();
+
+        my @client = $sth->fetchrow_array;
+        if (@client) {
+            my $session = CGI::Session->new();
+            $session->param("card_id", $client[0]);
+            $session->param("account_id", $client[1]);
+            $session->param("name", $client[2]);
+            $session->expire(time + $session_time);
+            $session->flush();
+
+            my $cookie = $cgi->cookie(-name => "session_id_cliente",
+                                    -value => $session->id(),
+                                    -expires => time + $session_time,
+                                    "-max-age" => time + $session_time);
+            print $cgi->header("text/xml", -cookie => $cookie);
+
+            return;
+        } else {
+            $errors{login} = "El número de tarjeta y la clave no coinciden.";
+        }
+    }
+
+    print_errors();
 }
 
+sub check_card_number {
+    my $card_number = $_[0];
+    my $length = length($card_number);
+    if (!$card_number || $length == 0) {
+        return "Ingrese un número de tarjeta.";
+    }
+    if ($card_number !~ /^\d{16}$/) {
+        return "Número de tarjeta no valido.";
+    }
+}
 
+sub check_password {
+    my $password = $_[0];
+    my $length = length($password);
+    if (!$password || $length == 0) {
+        return "Ingrese una clave.";
+    }
+    if ($length > 30) {
+        return "Clave muy larga.";
+    }
+}
+
+sub print_errors {
+    print "<errors>\n";
+    for my $key (keys %errors) {
+        if ($errors{$key}) {
+        print<<XML;
+    <error>
+        <element>$key</element>
+        <message>$errors{$key}</message>
+    </error>
+XML
+        }
+    }
+    print "</errors>\n";
+}
