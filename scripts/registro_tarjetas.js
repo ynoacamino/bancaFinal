5fe810242090ad3fa4359f379322dc294ac3da51
Christian Mestas Zegarra <cmestasz@unsa.edu.pe>
2024-02-06 09:35:14 -0500

    ultimas correcciones

--- scripts/registro_tarjetas.js
+++ scripts/registro_tarjetas.js
@@ -1,6 +1,6 @@
 function createCard() {
     // TODO: cards are only created with these now
-    fetch2(["number", "password", ["currency", "soles", "dolares"], "dni"], [], "registro_tarjetas.pl", function(response) {
+    fetch2(["number", "password", ["currency", "s", "d"], "dni"], [], "registro_tarjetas.pl", function(response) {
         const errors = response.getElementsByTagName("error");
         if (errors.length != 0) {
             // TODO: update these when the  v   htmls are done
